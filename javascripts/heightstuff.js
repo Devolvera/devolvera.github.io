a61b2776b6f7ac1e858731380815fb0597fa2053
Devolvera <donovan.olvera@gmail.com>
2014-06-12 17:25:46 -0700

    fix slide 1, add animate.css

--- javascripts/heightstuff.js
+++ javascripts/heightstuff.js
@@ -18,7 +18,7 @@ $('#slide-2').css({
 $.fn.center = function () {
    this.css("position","absolute");
    this.css("top", ( $(window).height() - this.height() ) / 2  + "px");
-   this.css("left", ( $(window).width() - this.width() ) / 2 + "px");
+/*   this.css("left", ( $(window).width() - this.width() ) / 2 + "px"); */
    return this;
 }
 
